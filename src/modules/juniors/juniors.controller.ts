5fc547808656fd99fe289ce36e999a5532e6f467
Igor Gonçalves <igordossantos38@gmail.com>
2025-05-09 23:40:50 -0300

    feat: cria novo enpoint, remove duplicação e move regra de negócio para service

--- src/modules/juniors/juniors.controller.ts
+++ src/modules/juniors/juniors.controller.ts
@@ -1,4 +1,16 @@
-import { BadRequestException, Body, Controller, HttpStatus, Post, Res , UseGuards, Get, Req, Param, NotFoundException} from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  HttpStatus,
+  Post,
+  Res,
+  UseGuards,
+  Get,
+  Req,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
 import { JuniorsService } from './juniors.service';
 import { CreateJuniorDto } from './dtos/create-junior-dto';
 import { JuniorEntity } from 'src/database/entities/junior.entity';
@@ -8,84 +20,96 @@ import { SecretKeyGuard } from 'src/shared/guards/secret-key.guard';
 
 @Controller('juniors')
 export class JuniorsController {
-  constructor(private readonly juniorsService: JuniorsService) { }
+  constructor(private readonly juniorsService: JuniorsService) {}
 
-  
   @ApiOperation({
-      summary: 'Registra o Junior no Banco de Dados',
-    })
+    summary: 'Registra o Junior no Banco de Dados',
+  })
   @ApiResponse({
-      status: 200,
-      description: 'Sucesso',
-      type: CreateJuniorDto,
-    })
+    status: 200,
+    description: 'Sucesso',
+    type: CreateJuniorDto,
+  })
   @ApiResponse({
-      status: 400,
-      description: 'Erro',
-      type: BadRequestException,
-    })
+    status: 400,
+    description: 'Erro',
+    type: BadRequestException,
+  })
   @ApiHeader({
-      name: 'x-api-key',
-      description: 'Chave secreta para autenticação'
+    name: 'x-api-key',
+    description: 'Chave secreta para autenticação',
   })
   @UseGuards(SecretKeyGuard)
   @Post()
-  async create(@Body() createJuniorDto: CreateJuniorDto, @Res() res: Response): Promise<JuniorEntity> {
-
-
+  async create(
+    @Body() createJuniorDto: CreateJuniorDto,
+    @Res() res: Response,
+  ): Promise<JuniorEntity> {
     try {
-      const juniorExist = await this.juniorsService.findJuniorByEmail(createJuniorDto.email);
+      const juniorExist = await this.juniorsService.findJuniorByEmail(
+        createJuniorDto.email,
+      );
 
       if (juniorExist) {
         res.status(HttpStatus.CONFLICT).json({
           statusCode: HttpStatus.CONFLICT,
-          message: 'Já existe um junior com esse email.'
-        })
-        return
+          message: 'Já existe um junior com esse email.',
+        });
+        return;
       }
 
       const junior = await this.juniorsService.create(createJuniorDto);
 
-      if(!junior){
-        res.status(HttpStatus.BAD_REQUEST).json({mensagem: 'junior não foi criado'})
+      if (!junior) {
+        res
+          .status(HttpStatus.BAD_REQUEST)
+          .json({ mensagem: 'junior não foi criado' });
       }
 
-      res.status(HttpStatus.CREATED).json(junior)
-
+      res.status(HttpStatus.CREATED).json(junior);
     } catch (error) {
-      res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({message: "Erro interno"})
+      res
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .json({ message: 'Erro interno' });
     }
   }
 
   @ApiOperation({
-    summary: "Resgata registro do junior no Banco"
+    summary: 'Resgata registro do junior no Banco',
   })
   @ApiResponse({
     status: 200,
-    description: "Sucesso",
-    type: CreateJuniorDto
-  })
-  @ApiResponse({
-    status: HttpStatus.NOT_FOUND,
-    description: "Junior não encontrado",
-    type: NotFoundException
+    description: 'Sucesso',
+    type: CreateJuniorDto,
   })
   @ApiResponse({
     status: 400,
-    description: "Erro",
+    description: 'Erro',
     type: BadRequestException,
   })
-  @Get(":email")
-  async getByEmail(@Param('email') email: string): Promise<CreateJuniorDto>{
-      if(!email || !email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)){
-        throw new BadRequestException("Email inválido.");
-      }
+  @Get(':email')
+  async getByEmail(@Param('email') email: string): Promise<CreateJuniorDto> {
+    const junior = await this.juniorsService.findJuniorByEmail(email);
 
-      const junior = await this.juniorsService.findJuniorByEmail(email);
+    return junior;
+  }
 
-      if(!junior){
-        throw new NotFoundException("Junior não encontrado");
-      }
-      return junior;
+  @ApiOperation({
+    summary: 'Resgata todos os juniors do Banco',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Sucesso',
+    type: CreateJuniorDto,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Erro',
+    type: BadRequestException,
+  })
+  @Get('/')
+  async getAll(): Promise<CreateJuniorDto[]> {
+    const juniors = await this.juniorsService.findAll();
+    return juniors;
   }
 }
--- src/modules/juniors/juniors.service.ts
+++ src/modules/juniors/juniors.service.ts
@@ -1,36 +1,49 @@
-import { HttpException, HttpStatus, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateJuniorDto } from './dtos/create-junior-dto';
 import { JuniorMDBEntity } from 'src/database/entities/juniormdb.mongo-entity';
 
-
 @Injectable()
 export class JuniorsService {
-    constructor(
-        @InjectRepository(JuniorMDBEntity, 'mongoConnection')
-        private readonly juniormdbRepository: Repository<JuniorMDBEntity>
-    ) { }
+  constructor(
+    @InjectRepository(JuniorMDBEntity, 'mongoConnection')
+    private readonly juniormdbRepository: Repository<JuniorMDBEntity>,
+  ) {}
 
-    async create(createJuniorDto: CreateJuniorDto): Promise<JuniorMDBEntity> {
+  async create(createJuniorDto: CreateJuniorDto): Promise<JuniorMDBEntity> {
+    const juniorMDB = this.juniormdbRepository.create({
+      ...createJuniorDto,
+      startDate: new Date(createJuniorDto.startDate),
+    });
 
-        const juniorMDB = this.juniormdbRepository.create({
-            ...createJuniorDto,
-            startDate: new Date(createJuniorDto.startDate)
-        });
+    return this.juniormdbRepository.save(juniorMDB);
+  }
 
-
-
-        return this.juniormdbRepository.save(juniorMDB);
+  async findJuniorByEmail(email: string): Promise<JuniorMDBEntity> {
+    if (!email || !email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
+      throw new BadRequestException('Email inválido.');
     }
 
-    async findJuniorByEmail(email: string): Promise<JuniorMDBEntity> {
-        const junior = await this.juniormdbRepository.findOne({where: { email }});
-
-        if (!junior) {
-            return null;
-        }
-
-        return junior;
+    const junior = await this.juniormdbRepository.findOne({ where: { email } });
+    if (!junior) {
+      throw new NotFoundException('Junior não encontrado.');
     }
+
+    return junior;
+  }
+
+  async findAll(): Promise<JuniorMDBEntity[]> {
+    const juniors = await this.juniormdbRepository.find();
+
+    if (!juniors) {
+      throw new NotFoundException('Nenhum dado encontrado.');
+    }
+
+    return juniors;
+  }
 }
